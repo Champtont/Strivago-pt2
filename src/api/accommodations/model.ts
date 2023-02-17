39816d47d625fde4df35feec637c008196ffc571
Courtney Hampton <jaye0888@gmail.com>
2023-02-17 14:57:27 +0100

    Hw is going...

---
+++ src/api/accommodations/model.ts
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+import { Document, Model, Schema } from "mongoose";
+
+interface Accomodation extends Document {
+  name: string;
+  host: Schema.Types.ObjectId;
+  description: string;
+  maxGuests: number;
+  city: string;
+  country: string;
+}
+
+const accomodationSchema: Schema<Accomodation> = new Schema(
+  {
+    name: { type: String, required: true },
+    host: { type: Schema.Types.ObjectId, ref: "users", required: true },
+    description: { type: String, required: true },
+    maxGuests: { type: Number, required: true },
+    city: { type: String, required: true },
+    country: { type: String, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const accomodationModel: Model<Accomodation> = mongoose.model(
+  "accomodation",
+  accomodationSchema
+);
+
+export default accomodationModel;
---
+++ src/server.ts
@@ -0,0 +1,25 @@
+import express from "express";
+import cors from "cors";
+import usersRouter from "./api/users";
+import accomRouter from "./api/accommodations";
+import {
+  badRequestHandler,
+  unauthorizedHandler,
+  genericErrorHandler,
+  forbiddenHandler,
+} from "./errorHandlers";
+
+const server = express();
+
+server.use(cors());
+server.use(express.json());
+
+server.use("/users", usersRouter);
+server.use("/accomodation", accomRouter);
+
+server.use(badRequestHandler);
+server.use(unauthorizedHandler);
+server.use(forbiddenHandler);
+server.use(genericErrorHandler);
+
+export default server;
